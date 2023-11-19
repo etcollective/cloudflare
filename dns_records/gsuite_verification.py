d12faa0555131802e41d01b42af4d3cf31c526c8
Christian Diaz <christian@cdiaz.cloud>
2023-11-19 14:52:00 -0700

    IT-7: Add CNAME for Google Workspace

--- dns_records/gsuite_verification.py
+++ dns_records/gsuite_verification.py
@@ -1,6 +1,12 @@
 import pulumi
 import pulumi_cloudflare as cf
+
 from dns_records import zone
+
+# Setup Config / Vars
+config = pulumi.Config('google_workspace')
+verification_host = config.require_secret('verification_host')
+verification_dest = config.require_secret('verification_dest')
 
 # Begin Records
 support_verification = cf.Record(
@@ -10,5 +16,15 @@ support_verification = cf.Record(
     value='google.com',
     ttl=3600,
     type='CNAME',
+    opts=pulumi.ResourceOptions(parent=zone, delete_before_replace=True),
+)
+
+workspace_verification = cf.Record(
+    'google-workspace-verfication-record',
+    zone_id=zone.id,
+    name=verification_host,
+    type='CNAME',
+    value=verification_dest,
+    ttl=3600,
     opts=pulumi.ResourceOptions(parent=zone, delete_before_replace=True),
 )
