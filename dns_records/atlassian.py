892366e2ff5154f341433bd400ab8546015dd31e
Christian Diaz <christian@cdiaz.cloud>
2023-11-22 11:11:52 -0700

    IT-15: Add Mail Verification TXTs

--- dns_records/atlassian.py
+++ dns_records/atlassian.py
@@ -12,3 +12,39 @@ atlassian_verification = cf.Record(
     proxied=False,
     zone_id=zone.id,
 )
+
+
+dkim_values = [
+    {
+        'name': 'atlassian-042b54._domainkey.etcollective.org',
+        'value': 'atlassian-042b54.dkim.atlassian.net',
+    },
+    {
+        'name': 'atlassian-36dbe2._domainkey.etcollective.org',
+        'value': 'atlassian-36dbe2.dkim.atlassian.net',
+    },
+    {
+        'name': 'atlassian-bounces.etcollective.org',
+        'value': 'bounces.mail-us.atlassian.net',
+    },
+]
+
+for dkim in dkim_values:
+    cf.Record(
+        f'atlassian-dkim-dns-record-{dkim["name"]}',
+        name=dkim['name'],
+        ttl=86400,
+        type='CNAME',
+        value=dkim['value'],
+        proxied=False,
+        zone_id=zone.id,
+    )
+
+txt_mail = cf.Record(
+    'atlassian-mail-txt-record',
+    name='@',
+    value='atlassian-sending-domain-verification=1ab91f15-080c-40b2-b4b9-503cd2d0e70b',
+    type='TXT',
+    proxied=False,
+    zone_id=zone.id,
+)
